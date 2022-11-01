2ed5711d449ab581a07c6458089dcf08bd9c26a9
Tharcius <tharciussilva@gmail.com>
2022-10-31 23:59:47 -0300

    Implementando Observer

--- companies/app/Providers/AppServiceProvider.php
+++ companies/app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Company;
+use App\Observers\CompanyObserver;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +25,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Company::observe(CompanyObserver::class);
     }
 }
